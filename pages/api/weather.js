2aece15cdf49a21c870aee93b2baf8ce61662b76
Zeljko Dakic <zeljko@dakic.com>
2020-10-25 23:34:09 -0500

    return sorted by day

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { join } from 'path'
+import { _ } from 'lodash'
 
 const dataDir = join(process.cwd(), 'data')
 const fullPath = join(dataDir, 'weather-response.json')
@@ -10,9 +11,17 @@ export default function handler(req, res) {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   if (data["cod"] == "200") {
-    const r = data["list"];
+    const r = transforPerDay(data["list"]);
     res.end(JSON.stringify(r))  
   } else {
     res.end(JSON.stringify({"message": "womp, womp, womp"}))
   }
+}
+
+function transforPerDay(data) {
+  var r = _(data)
+    .groupBy((e) => { 
+      return _.split(e["dt_txt"], " ")[0]
+    })
+  return r;
 }
